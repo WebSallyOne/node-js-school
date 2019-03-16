b97d960f09ecedbb029f5b402f8f829f8a707eb8
SallyOne <thxrobert@gmail.com>
2019-03-16 18:32:02 +0200

    Added: new route file for service

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import * as Router from 'koa-router';
 import controller = require('../controller');
 import { userRouter } from './users';
+import { serviceRouter } from './services';
 
 const router = new Router();
 
@@ -8,5 +9,6 @@ const router = new Router();
 router.get('/', controller.general.helloWorld);
 router.get('/jwt', controller.general.getJwtPayload);
 router.use('/users', userRouter.routes(), userRouter.allowedMethods());
+router.use('/service', serviceRouter.routes(), userRouter.allowedMethods());
 
 export { router };
